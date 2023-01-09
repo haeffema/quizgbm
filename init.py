2e1d8eef0300b5bb2cd04c2ffc13147a892d88cd
haeffema <haeffema@gmail.com>
2023-01-09 20:39:07 +0100

    updates init py

---
+++ init.py
@@ -0,0 +1,9 @@
+from player import Player
+
+players = [Player(360366344635547650, "Jan"),
+           Player(210087557654904832, "Luis"),
+           Player(360368525379895298, "Julian"),
+           Player(502837161326936075, "Tim"),
+           Player(762297951862128640, "Lara")]
+
+questions = []
