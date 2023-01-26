7ae4468a36fc41a0d56d5b7240fbfdfcd25f6e9c
mhae03 <maximilian.haeffele@sw-machines.com>
2023-01-26 11:44:02 +0100

    ready for test run

--- creds.py
+++ creds.py
@@ -1,4 +1,4 @@
 bot_token = "ODAyMjE5Mjk0MzMwNTg1MTQ4.GQCiWU.vCzbHsvbqzdNNuf0S0PLZkoAPMuerP57CYjP3E"
-folder = 'lotr_max'
+folder = 'lotr_max_test'
 quiz_master_id = 326305842427330560
-quiz_channel_id = 1068113569851383849
+quiz_channel_id = 1032314542186823730
