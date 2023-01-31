75b279e51099674a3f63f0337f44353c7563fe6d
mhae03 <maximilian.haeffele@sw-machines.com>
2023-01-31 15:44:16 +0100

    user correct quiz

--- creds.py
+++ creds.py
@@ -1,4 +1,4 @@
 bot_token = "ODAyMjE5Mjk0MzMwNTg1MTQ4.GQCiWU.vCzbHsvbqzdNNuf0S0PLZkoAPMuerP57CYjP3E"
-folder = 'lotr_max_test'
+folder = 'lotr_max'
 quiz_master_id = 326305842427330560
 quiz_channel_id = 1032314542186823730
