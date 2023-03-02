27cf9044b8741a062bc27ffc140c260224d5612d
haeffema <haeffema@gmail.com>
2023-03-02 10:55:40 +0100

    ids

--- creds.py
+++ creds.py
@@ -2,3 +2,5 @@ bot_token = "ODAyMjE5Mjk0MzMwNTg1MTQ4.GQCiWU.vCzbHsvbqzdNNuf0S0PLZkoAPMuerP57CYj
 folder = 'resources\\lotr_max'
 quiz_master_id = 326305842427330560
 quiz_channel_id = 1032314542186823730
+table = 1080168567732969624
+log = 1080168548745351218
