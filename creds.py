fb91182c4d3b0b4f19e4d288a9d4c3de74da2aa8
mhae03 <maximilian.haeffele@sw-machines.com>
2023-03-03 11:04:07 +0100

    ready version

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 bot_token = "ODAyMjE5Mjk0MzMwNTg1MTQ4.GQCiWU.vCzbHsvbqzdNNuf0S0PLZkoAPMuerP57CYjP3E"
 folder = 'resources\\bunt_gemischt'
 quiz_master_id = 326305842427330560
-quiz_channel_id = 1081152526608519188
-table_channel_id = 1081152526608519188
-log_channel_id = 1081152526608519188
+quiz_channel_id = 1032314542186823730
+table_channel_id = 1080168567732969624
+log_channel_id = 1080168548745351218
